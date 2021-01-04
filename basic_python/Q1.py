12df5a964e04f0a0b748d94436361ad44c63fa48
Shardul Patil <patilshardulsatish@yahoo.com>
2021-01-04 22:42:39 +0530

    Add : Create a Q1 module to reverse user name

---
+++ basic_python/Q1.py
@@ -0,0 +1,26 @@
+class Q1:
+    def __init__(self):
+        pass
+    def get_user_name(self):
+        
+        first_name=input("Enter User Name")
+        last_name=input("Enter User Name")
+        self.reverse_user_name(first_name,last_name)
+
+    def reverse_user_name(self,first_name,last_name):    
+        
+        user_name = list()
+
+        first_part=list(first_name)
+        first_part.reverse()
+        last_part=list(last_name)
+        last_part.reverse()
+        
+        user_name.append(last_part)
+        user_name.append(first_part)
+        
+        print(user_name)
+        
+if __name__ == "__main__":
+    q1 = Q1()
+    q1.get_user_name()
