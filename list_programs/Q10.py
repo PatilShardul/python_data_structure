cacc109f0ac9b1b00e26aa3568bcb7fe20d39860
Shardul Patil <patilshardulsatish@yahoo.com>
2021-01-05 09:29:54 +0530

    Add : Created a module to get list specific elements from a given list

--- list_programs/Q10.py
+++ list_programs/Q10.py
@@ -1,7 +1,14 @@
+''' 
+  @Author: Shardul Patil
+  @Date: 2021-01-05 09:27:33
+  @Last Modified by:   Shardul Patil
+  @Last Modified time: 2021-01-05 09:27:33  
+'''
 class Q10:
     def __init__(self):
         self.sample_list = ['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow']
         self.output_list = list()
+    
     def truncate_list(self):
 
         self.sample_list.pop(5)
