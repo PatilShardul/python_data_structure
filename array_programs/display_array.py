54af511e8985ae86a3b97514f9e9b3558f213aa6
Shardul Patil <patilshardulsatish@yahoo.com>
2021-01-08 16:26:30 +0530

    Refactor : coded statement to call display array method

--- array_programs/display_array.py
+++ array_programs/display_array.py
@@ -3,20 +3,21 @@
   @Date: 2021-01-08 16:13:40
   @Last Modified by:   Shardul Patil
   @Last Modified time: 2021-01-08 16:13:40  
-  @Description : Python program to create an array of 5 integers and display the array items. Access individual element through indexes
+  @Description : Python program to reverse the order of the items in the array. 
 '''
 class DisplayArray:
     def get_array_elements(self):
         """This Method Takes comma seperated input elements from user andf store in array list
         """
         self.array_list=[eval(element) for element in input("Enter the Array Elements : ").split(",")]
-        print(self.array_list)
+        self.print_array_elements()
+
 
     def print_array_elements(self):
         """This menthod prints a array elements
         """
         for index in range(0,len(self.array_list)):
-            print(self.array_list[index])
+            print(self.array_list[index],end=" ")
 
 if __name__ == "__main__":
     display=DisplayArray()
