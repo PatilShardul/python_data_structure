aeb39089ec13226360672bf09a0316d719d0e719
Shardul Patil <patilshardulsatish@yahoo.com>
2021-01-08 10:32:40 +0530

    Add : Created a Python program to find the available built-in modules

---
+++ basic_python/Q22.py
@@ -0,0 +1,14 @@
+''' 
+  @Author: Shardul Patil
+  @Date: 2021-01-08 10:31:32
+  @Last Modified by:   Shardul Patil
+  @Last Modified time: 2021-01-08 10:31:32  
+  @Descript: Created a program to get built in method in python 
+'''
+
+class BuiltInModules:
+    def __init__(self):
+        help('modules')
+        
+if __name__ == "__main__":
+    built_in_modules=BuiltInModules()            
